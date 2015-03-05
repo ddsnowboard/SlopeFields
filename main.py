bc32fbde6243e56286793500e776a5c24a661867
ddsnowboard <ddsnowboard@gmail.com>
2015-03-05 17:21:52 -0600

    Finished sorta
    See Comment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import turtle
-def drawCoordinates():
+from math import atan, tan, cos, acos, sin, asin, pi, e
+# Make the dimensions editable. 
+def draw(equation):
 	TICK_MARK_LENGTH = 10
+	FIELD_LINE_LENGTH = 18
 	turtle.pu()
 	turtle.goto(turtle.window_width()/-2, 0)
 	turtle.setheading(0)
@@ -18,7 +21,19 @@ def drawCoordinates():
 		turtle.pd()
 		turtle.forward(TICK_MARK_LENGTH)
 	for i in range(1, 21):
-		
-# eqn = eval("lambda x: "+input("Input your equation in proper python with x as variable: "))
+		turtle.pu()
+		turtle.goto(TICK_MARK_LENGTH/2, (turtle.window_height()/2)-((turtle.window_height()/20)*i), )
+		turtle.seth(180)
+		turtle.pd()
+		turtle.forward(TICK_MARK_LENGTH)
+	for x in range(-10, 11):
+		for y in range(-10, 11):
+			turtle.pu()
+			turtle.goto((turtle.window_width()/20)*x, (turtle.window_height()/20)*y)
+			turtle.seth((180/pi)*(atan(equation(x, y))))
+			turtle.forward(FIELD_LINE_LENGTH/2)
+			turtle.pd()
+			turtle.backward(FIELD_LINE_LENGTH)
+eqn = eval("lambda x, y: "+input("Input your equation in proper python with x and y as variables: "))
 turtle.speed(0)
-drawCoordinates()
+draw(eqn)
