bb2c0cea3d4d719289824facb2349f80265e4a98
ddsnowboard <ddsnowboard@gmail.com>
2015-03-05 21:54:03 -0600

    Try to fix some issues.
    It goes over the edge, and the lines don't line up on the vertical part.

--- main.py
+++ main.py
@@ -6,39 +6,43 @@ def draw(equation, dims):
 	TICK_MARK_LENGTH = 10
 	FIELD_LINE_LENGTH = 18
 	SPACE_FROM_LABEL = 15
+	width = turtle.window_width()-55
+	height = turtle.window_height()-30
 	turtle.pu()
-	turtle.goto(turtle.window_width()/-2, 0)
+	turtle.goto(width/-2, 0)
 	turtle.setheading(0)
 	turtle.pd()
-	turtle.forward(turtle.window_width())
+	turtle.forward(width)
 	turtle.pu()
-	turtle.goto(0, turtle.window_height()/2)
+	turtle.goto(0, height/2)
 	turtle.setheading(270)
 	turtle.pd()
-	turtle.forward(turtle.window_height())
-	for i in range(1, 21):
+	turtle.forward(height)
+	for i in range(21):
 		turtle.pu()
-		turtle.goto((turtle.window_width()/-2)+((turtle.window_width()/20)*i), TICK_MARK_LENGTH/2)
+		turtle.goto((width/-2)+((width/20)*i), TICK_MARK_LENGTH/2)
 		turtle.seth(270)
 		turtle.pd()
 		turtle.forward(TICK_MARK_LENGTH)
 		turtle.pu()
 		turtle.forward(SPACE_FROM_LABEL)
 		turtle.write((-1*dims['x']/2)+(dims['x']/20)*i, False, "center")
-	for i in range(1, 21):
+	for i in range(21):
 		turtle.pu()
-		turtle.goto(TICK_MARK_LENGTH/2, (turtle.window_height()/2)-((turtle.window_height()/20)*i), )
+		turtle.goto(TICK_MARK_LENGTH/2, (height/2)-((height/20)*i))
 		turtle.seth(180)
 		turtle.pd()
 		turtle.forward(TICK_MARK_LENGTH)
-		turtle.pu()
-		turtle.forward(SPACE_FROM_LABEL)
-		turtle.write((-1*dims['x']/2)+(dims['x']/20)*i, False, "center")
-	turtle.pen(pencolor="green")
-	for x in myRange(dims['x']/-2, (dims['x']/2+1), dims['x']/20):
-		for y in myRange(dims['y']/-2, dims['y']/2+1, dims['y']/20):
+		if i != 10:
+			turtle.left(25)
 			turtle.pu()
-			turtle.goto((turtle.window_width()/dims['x'])*x, (turtle.window_height()/dims['y'])*y)
+			turtle.forward(SPACE_FROM_LABEL)
+			turtle.write((dims['y']/2)-(dims['y']/20)*i, False, "center")
+	turtle.pen(pencolor="green")
+	for x in myRange(dims['x']/-2, (dims['x']/2), dims['x']/20):
+		for y in myRange(dims['y']/-2, dims['y']/2, dims['y']/20):
+			turtle.pu()
+			turtle.goto((width/dims['x'])*x, (width/dims['y'])*y)
 			turtle.seth((180/pi)*(atan(equation(x, y))))
 			turtle.forward(FIELD_LINE_LENGTH/2)
 			turtle.pd()
